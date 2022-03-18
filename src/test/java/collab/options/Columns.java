e559fcf43fca55a2f5b398db62b6920816956dec
이준훈 <trsprs@gmail.com>
2022-03-18 14:54:30 +0900

    성명의 성으로 검색하는 옵션 테스트 (Mock 생성) List로 리턴하는 것을 ArrayList로 수정

---
+++ src/test/java/collab/options/Columns.java
@@ -0,0 +1,11 @@
+package collab.options;
+
+public class Columns {
+
+  public static final String COLUMN_EMPLOYEE_NUM = "employeeNum";
+  public static final String COLUMN_NAME = "name";
+  public static final String COLUMN_CARRER_LEVEL = "cl";
+  public static final String COLUMN_PHONE_NUMBER = "phoneNum";
+  public static final String COLUMN_BIRTHDAY = "birthday";
+  public static final String COLUMN_CERTIFICATE = "certi";
+}
