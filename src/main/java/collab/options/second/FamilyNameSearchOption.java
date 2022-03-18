a04f91339c08d6e75d617f34a22ddbd34a6aa3ce
이준훈 <trsprs@gmail.com>
2022-03-18 15:50:56 +0900

    리턴을 ArrayList로 수정

--- src/main/java/collab/options/second/FamilyNameSearchOption.java
+++ src/main/java/collab/options/second/FamilyNameSearchOption.java
@@ -18,13 +18,13 @@ public class FamilyNameSearchOption {
     this.dao = dao;
   }
 
-  public List<String> getFilterList() {
+  public ArrayList<String> getFilterList() {
 
     // TODO: Mock으로 lambda 어떻게 해야 할지 몰라서 for문으로 변경했습니다. 나중에 lamba로 고칠 것
 //    return dao.stream().filter(
 //        name -> (name.split(" ")[1].compareTo(conditionList.get(1))) == 0)
 //        .collect(Collectors.toList());
-    List<String> list = new ArrayList<>();
+    ArrayList<String> list = new ArrayList<>();
     for(int index = 0; index < dao.size(); index++) {
       String familyName = dao.get(index).split(" ")[1];
       if(familyName.compareTo(conditionList.get(1)) == 0) {
