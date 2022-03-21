c2a6f3f40cdfbc20c18f7cc5d5c21176aac3908b
전형준 <jhjun37@gmail.com>
2022-03-21 10:19:58 +0800

    [Feat] Implement testcases for parser
     - Add empty parser class
     - Implement testcases command parser
     - Add EmptySecondOption class
     - Add NoneThirdOption class

---
+++ src/main/java/collab/options/third/NoneThirdOption.java
@@ -0,0 +1,11 @@
+package collab.options.third;
+
+import collab.AbstractFirstOption;
+import collab.AbstractThirdOption;
+import collab.Employee;
+
+import java.util.List;
+
+public class NoneThirdOption extends AbstractThirdOption {
+
+}
