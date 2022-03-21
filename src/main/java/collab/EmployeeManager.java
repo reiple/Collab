2b7edd61e09eb74b67fd7d306eb413b1184cc2bb
전형준 <jhjun37@gmail.com>
2022-03-21 22:43:44 +0800

    [Feat] Add empty functions for EmployeeManager
     - 파일 입출력 함수, parsing 및 command execution 함수 추가

--- src/main/java/collab/EmployeeManager.java
+++ src/main/java/collab/EmployeeManager.java
@@ -1,8 +1,30 @@
 package collab;
 
+import java.util.List;
+
 public class EmployeeManager  {
     private IDAO dao;
+    private CommandParser parser;
     public EmployeeManager(){
         this.dao = new EmployeeDAO();
+        dao.initDatabase();
+        parser = new CommandParser();
+    }
+
+    public List<String> getCommandStringListFromFile(String FilePath){
+        return null;
+    }
+
+    public List<String> parseCommandList(List<String> cmdStrList){
+        return null;
+    }
+
+    public String executeCommandList(List<String> cmdStrList){
+        return null;
+    }
+
+    public String saveExecutionResultToFile(String FilePath){
+        return null;
     }
 }
+
