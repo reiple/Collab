0cc7355444ee143858f3e98c819e1abead8cfdd8
김영상 <101778572+ysk1202@users.noreply.github.com>
2022-03-21 13:35:11 +0900

    Update ModifyTest.java

--- src/test/java/collab/ModifyTest.java
+++ src/test/java/collab/ModifyTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import collab.DataBase;
-import collab.Employee;
 
 class ModifyTest {
   
