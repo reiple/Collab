330551e10079145a0ba08db8ce68b7281fd9d269
전형준 <jhjun37@gmail.com>
2022-03-22 01:31:12 +0800

    [Feat] EmployeeManager 구현
    - FileManager 객체 추가 구현 필요

--- src/main/java/collab/EmployeeManager.java
+++ src/main/java/collab/EmployeeManager.java
@@ -7,23 +7,29 @@ public class EmployeeManager  {
     private CommandParser parser;
     public EmployeeManager(){
         this.dao = new EmployeeDAO();
-        dao.initDatabase();
-        parser = new CommandParser();
+        this.dao.initDatabase();
+        this.parser = new CommandParser();
     }
 
     public List<String> loadCommandStringListFromFile(String filePath){
+        //FileManager 구현 필요
         return null;
     }
 
-    public List<ICommand> parseCommandList(List<String> commandStrList){
-        return null;
+    public List<ICommand> parseCommandList(List<String> commandStrList) throws Exception{
+        return this.parser.parseToCommandList(commandStrList);
     }
 
-    public String executeCommandList(List<ICommand> commandList){
-        return null;
+    public String executeCommandList(List<ICommand> commandList) throws Exception{
+        String executionResult = "";
+        for (ICommand command : commandList){
+            executionResult += command.executeCommand(this.dao);
+        }
+        return executionResult;
     }
 
-    public String saveExecutionResultToFile(String filePath, String excutionResult){
+    public String saveExecutionResultToFile(String filePath, String executionResult){
+        //FileManager 구현 필요
         return null;
     }
 }
