74d92e115c61a738e396e6f774dce64ce7a156f0
이준훈 <trsprs@gmail.com>
2022-03-21 21:25:50 +0900

    전화번호로 찾아서 이름 수정 명령어 구현

--- src/main/java/collab/ModifyCommand.java
+++ src/main/java/collab/ModifyCommand.java
@@ -1,8 +1,11 @@
 package collab;
 
+import collab.options.second.NoneSecondOption;
 import collab.options.third.NoneThirdOption;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ModifyCommand extends AbstractCommand{
     public ModifyCommand(AbstractFirstOption option1, AbstractSecondOption option2, List<String> commandArguments) {
@@ -10,7 +13,25 @@ public class ModifyCommand extends AbstractCommand{
     }
 
     @Override
-    public String executeCommand(EmployeeDAO dao) {
-        return null;
+    public String executeCommand(EmployeeDAO employeeDAO) {
+
+        if(getSecondOption() instanceof NoneSecondOption) {
+            return getValues(employeeDAO);
+        }
+
+        List<Employee> list = getSecondOption().getFilteredList(employeeDAO);
+        if(list == null) {
+            list = new ArrayList<>();
+        }
+
+        return getFirstOption().getFilteredList(list);
+    }
+
+    private String getValues(EmployeeDAO employeeDAO) {
+        List<Employee> list = employeeDAO.getAllItems().stream()
+            .filter(item -> item.getPhoneNumber().equals(getCommandArguments().get(1)))
+            .collect(Collectors.toList());
+
+        return getFirstOption().getFilteredList(list);
     }
 }
