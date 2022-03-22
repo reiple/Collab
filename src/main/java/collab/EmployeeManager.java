f19bd162bfb319a8d50826c0eead711e33d8675d
전형준 <Alphapos7@gmail.com>
2022-03-22 12:11:58 +0800

    [Feat] EmployeeManager 내 FileManager 연동

--- src/main/java/collab/EmployeeManager.java
+++ src/main/java/collab/EmployeeManager.java
@@ -5,15 +5,17 @@ import java.util.List;
 public class EmployeeManager  {
     private IDAO dao;
     private CommandParser parser;
+    private FileManager fileManager;
     public EmployeeManager(){
         this.dao = new EmployeeDAO();
         this.dao.initDatabase();
         this.parser = new CommandParser();
+        this.fileManager = new FileManager();
     }
 
-    public List<String> loadCommandStringListFromFile(String filePath){
+    public List<String> loadCommandStringListFromFile(String filePath) throws Exception{
         //FileManager 구현 필요
-        return null;
+        return this.fileManager.loadFile(filePath);
     }
 
     public List<ICommand> parseCommandList(List<String> commandStrList) throws Exception{
@@ -28,9 +30,9 @@ public class EmployeeManager  {
         return executionResult;
     }
 
-    public String saveExecutionResultToFile(String filePath, String executionResult){
+    public void saveExecutionResultToFile(String filePath, String executionResult) throws Exception{
         //FileManager 구현 필요
-        return null;
+        this.fileManager.saveFile(filePath, executionResult);
     }
 }
 
