2adf7eee5e30b18627efb44f816d90ed844f9e88
전형준 <Alphapos7@gmail.com>
2022-03-22 15:31:38 +0800

    [Feat] Main client code 구현
    - Test code 는 따로 구현하지 않음
    - EmployManager 내 File to File 최종 test 존재. 리팩토리시 수정 필요.

--- src/main/java/collab/Main.java
+++ src/main/java/collab/Main.java
@@ -1,9 +1,29 @@
 package collab;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-
+        if (args.length !=2) {
+            System.out.println("Usage: Main.exe input.txt output.txt");
+        }
+        String inFilePath = args[0];
+        //System.out.println("in: " + inFilePath);
+        String outFilePath = args[1];
+        //System.out.println("out: " + outFilePath);
+        try {
+            EmployeeManager employeeManager = new EmployeeManager();
+            List<String> commandStringList = employeeManager.loadCommandStringListFromFile(inFilePath);
+            //System.out.println("cmdStrList: " + commandStringList);
+            List<ICommand> commandList = employeeManager.parseCommandList(commandStringList);
+            //System.out.println("cmdList: " + commandList);
+            String executionResult = employeeManager.executeCommandList(commandList);
+            //System.out.println("exeResult: " + executionResult);
+            employeeManager.saveExecutionResultToFile(outFilePath, executionResult);
+        }catch(Exception e){
+            System.out.println("Error occurred!!!");
+            System.out.println(e);
+        }
     }
 }
