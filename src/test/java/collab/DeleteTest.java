6b26370918ce22649f67c5f67a1f48d94fead15f
김영상 <101778572+ysk1202@users.noreply.github.com>
2022-03-22 17:42:45 +0900

    Update DeleteTest.java

--- src/test/java/collab/DeleteTest.java
+++ src/test/java/collab/DeleteTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import options.Columns;
 
 class DeleteTest {
   
