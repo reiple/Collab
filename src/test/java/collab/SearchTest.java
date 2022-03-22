e70c9ed264bdb6a4c04c3a8607f2cbeeb5f7981c
ysk1202 <youngsang2@naver.com>
2022-03-22 11:56:39 +0900

    [Fix] 유효하지 않은 Input 정보 수정

--- src/test/java/collab/SearchTest.java
+++ src/test/java/collab/SearchTest.java
@@ -9,84 +9,79 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SearchTest {
+  
   DataBase empDB = new DataBase();
+  Employee answer;
+  Employee noAnswer;
+  
   @BeforeEach
   public void setUp() {
     empDB.add(new Employee(Arrays.asList("15123099","VXIHXOTH JHOP","CL3","010-3112-2609","19771211","ADV")));
     empDB.add(new Employee(Arrays.asList("17112609","FB NTAWR","CL4","010-5645-6122","19860903","PRO")));
     empDB.add(new Employee(Arrays.asList("18115040","TTETHU HBO","CL3","010-4581-2050","20080718", "ADV")));
+    
+    answer = new Employee(Arrays.asList("17112609","FB NTAWR","CL4","010-5645-6122","19860903","PRO"));
+    noAnswer = new Employee(Arrays.asList("19000000","TEST TEST","CL2","010-0000-0000","19000101","EX"));
   }
   
   public int searchTest_EmployeeNum(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
-    empList = empDB.searchItems("employeeNum", answer.getEmployeeNumber());
+    List<Employee> empList = empDB.searchItems("employeeNum", answer.getEmployeeNumber());
+
     if(empList.size()==1) {
-      resultCount++;
       assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
     }
-    return resultCount;// 0 or 1
-  }
+    return 0;
+  }   
   
   public int searchTest_Name(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
+    List<Employee> empList = empDB.searchItems("name", answer.getName());
 
-    empList = empDB.searchItems("name", answer.getName());
     if(empList.size()==1) {
-      resultCount++;
       assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
     }
-
-    return resultCount;
-    
+    return 0;
   }    
 
   public int searchTest_Cl(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
-    empList = empDB.searchItems("cl", answer.getCareerLevel());
-    if(empList.size()==1) {
-      resultCount++;
-      assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
-    }
+    List<Employee> empList = empDB.searchItems("cl", answer.getCareerLevel());
 
-    return resultCount;// 0 or 1
+    if(empList.size()==1) {
+      assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
+    }
+    return 0;
   }
 
   public int searchTest_PhoneNum(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
-    
-    empList = empDB.searchItems("phoneNum", answer.getPhoneNumber());
+    List<Employee> empList = empDB.searchItems("phoneNum", answer.getPhoneNumber());
+
     if(empList.size()==1) {
-      resultCount++;
       assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
     }
-    return resultCount;// 0 or 3
+    return 0;
   }
 
   public int searchTest_BirthDay(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
-    
-    empList = empDB.searchItems("birthday", answer.getBirthday());
+    List<Employee> empList = empDB.searchItems("birthday", answer.getBirthday());
+
     if(empList.size()==1) {
-      resultCount++;
       assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
     }
-    return resultCount;// 0 or 4
+    return 0;
   }
   
   public int searchTest_Certi(Employee answer) {
-    List<Employee> empList;
-    int resultCount=0;
-    empList = empDB.searchItems("certi", answer.getCerti());
+    List<Employee> empList = empDB.searchItems("certi", answer.getCerti());
+
     if(empList.size()==1) {
-      resultCount++;
       assertEquals(empList.get(0).getEmployeeNumber(), answer.getEmployeeNumber());
+      return 1;
     }
-    return resultCount;// 0 or 1
+    return 0;
   }
   
   public int searchTest_Total(Employee answer) {
@@ -97,20 +92,15 @@ class SearchTest {
     searchTest_BirthDay(answer)+
     searchTest_Certi(answer);
   }
-  
+
   @Test
   public void searchTestFound() {
-    Employee answer = new Employee(Arrays.asList("17112609","FB NTAWR","CL4","010-5645-6122","19860903","PRO"));
     assertTrue(searchTest_Total(answer)==6);
   }
   
   @Test
   public void searchTestNotFound() {
-    assertThrows(RuntimeException.class, () -> {
-      Employee noAnswer = new Employee(Arrays.asList("19000000","TEST TEST","CL5","010-0000-0000","19000101","TEST"));
-      assertTrue(searchTest_Total(noAnswer)==0);
-    });
-
+    assertTrue(searchTest_Total(noAnswer)==0);
   }
 }
 
