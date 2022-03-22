4788b25776da9b0b17c76cdb1e6236b6f7a7f856
ysk1202 <youngsang2@naver.com>
2022-03-22 18:49:33 +0900

    [Sytle] DAO Return 모두 동일하게 처리, 불필요 추상CLASS 삭제, TC 반영 안되어 재상신

--- src/main/java/collab/DataBase.java
+++ src/main/java/collab/DataBase.java
@@ -2,23 +2,32 @@ package collab;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-import collab.Employee;
 
 public class DataBase {
-  private ArrayList<Employee> employeeData = new ArrayList<Employee>();
+  HashSet<String> registerEmployeeNum = new HashSet<String>();
+  public ArrayList<Employee> employeeData = new ArrayList<Employee>();
   
   public ArrayList<Employee> getEmployeeAllData() {
     return employeeData;
   }
   
   public void add(Employee employee) {
-    employeeData.add(employee);        
+    if(registerEmployeeNum.contains(employee.getEmployeeNumber())) {
+      System.out.println(employee.getEmployeeNumber() + "Register Fail!! Employee Data Duplicated!!");
+      return;
+    }
+    registerEmployeeNum.add(employee.getEmployeeNumber());
+    employeeData.add(employee);
   }
-
+  
+  
   public Employee searchItem(String id) {
-    return searchItems("employeeNum", id).get(0);
+    List<Employee> foundItems = searchItems("employeeNum", id); 
+    if(foundItems == null) return null;
+    return foundItems.get(0);
   }
 
   public List<Employee> searchItems(String field, String value) {
@@ -26,32 +35,36 @@ public class DataBase {
       resultItem.addAll(employeeData.stream()
               .filter(item -> item.getField(field).equals(value))
               .collect(Collectors.toList()));
+      if(resultItem.size()==0) {
+        System.out.println("(field:"+field +" value : "+value+ ") can't find!!");
+        return null;
+      }
       return resultItem;
   }
   
   public Employee modifyItem(Employee employee) {
-    Employee foundItem = searchItems("employeeNum", employee.getEmployeeNumber()).get(0);
-    Employee returnItem = makeEmployee(foundItem);
-    foundItem = employee;
-    
-    return returnItem;
+    Employee foundItem = searchItem(employee.getEmployeeNumber());
+    if(foundItem == null) return null;
+    return makeEmployee(foundItem);
   }
   
   public Employee modifyItemById(String id, String field, String value) {
-    Employee foundItem = modifyItemByCondition("employeeNum", id, field, value).get(0);
-    return foundItem;
+    List<Employee> foundItems = modifyItemByCondition("employeeNum", id, field, value);
+    if(foundItems == null) return null;
+    return foundItems.get(0);
   }
   
   public List<Employee> modifyItemByCondition(String sField, String sValue, String cField, String cValue) {
-    List<Employee> returnItem = new ArrayList<Employee>();
+    List<Employee> foundItems = searchItems(sField, sValue);
+    if(foundItems==null) return null;
     
-    List<Employee> foundItem = searchItems(sField, sValue);
-    for(Employee employee : foundItem) {
-      returnItem.add(makeEmployee(employee));
+    List<Employee> returnItems = new ArrayList<Employee>();
+    for(Employee employee : foundItems) {
+      returnItems.add(makeEmployee(employee));
       modify(employee, cField, cValue);
     }
     
-    return returnItem;
+    return returnItems;
   }
   
   public void modify(Employee employee, String field, String value) {
@@ -68,14 +81,17 @@ public class DataBase {
   }
   
   public List<Employee> deleteItemByCondition(String field, String value) {
-    List<Employee> returnEmployee = new ArrayList<Employee>();
+    List<Employee> foundItems = searchItems(field, value);
+    if(foundItems==null) return null;
     
-    List<Employee> targetEmployee = searchItems(field, value);
-    for(Employee employee : targetEmployee) {
-      returnEmployee.add(makeEmployee(employee));
+    List<Employee> returnItems = new ArrayList<Employee>();
+    for(Employee employee : foundItems) {
+      returnItems.add(makeEmployee(employee));
+      registerEmployeeNum.remove(employee.getEmployeeNumber());
       employeeData.remove(employee);
     }
-    return returnEmployee;
+    
+    return returnItems;
   }
    
   
